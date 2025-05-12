a82f505f5dbbb9278c360d3bb61d499673faae65
Buenaventura123 <202310536@fit.edu.ph>
2025-05-12 17:28:43 +0800

    style: maked an indicator to the navbar

--- components/navbar.component.php
+++ components/navbar.component.php
@@ -1,7 +1,5 @@
-<nav style="background: #333; padding: 1rem;">
-    <ul style="display: flex; list-style: none; gap: 1rem; margin: 0; padding: 0;">
-        <li><a href="/AD-Task-1/page/home/index.php" style="color: white; text-decoration: none;">Home</a></li>
-        <li><a href="/AD-Task-1/page/high-rated/index.php" style="color: white; text-decoration: none;">High Rated</a></li>
-        <li><a href="/AD-Task-1/page/low-rated/index.php" style="color: white; text-decoration: none;">Low Rated</a></li>
-    </ul>
-</nav>
+<div class="navbar">
+    <a href="/AD-Task-1/page/home/index.php">All Movies</a>
+    <a href="/AD-Task-1/page/high-rated/index.php">High Rated</a>
+    <a href="/AD-Task-1/page/low-rated/index.php">Low Rated</a>
+</div>
