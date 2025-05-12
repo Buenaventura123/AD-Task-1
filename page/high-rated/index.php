1e6731e411f04889f849f67bac1cd12b356cc6ad
Buenaventura123 <202310536@fit.edu.ph>
2025-05-12 15:45:59 +0800

    refactor perf(card): added a containr to the card in high and low rated page card

--- page/high-rated/index.php
+++ page/high-rated/index.php
@@ -6,22 +6,27 @@ include '../../components/navbar.component.php';
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="../../assets/css/style.css">
+    <meta charset="UTF-8">
     <title><?= $title ?></title>
+    <link rel="stylesheet" href="../../assets/css/style.css">
 </head>
 <body>
-    <div class="movie-list">
-        <?php foreach ($movies as $movie): ?>
-            <?php if ($movie['rating'] > 7): ?>
+
+<div class="movie-list">
+    <?php foreach ($movies as $movie): ?>
+        <?php if ($movie['rating'] > 7): ?>
+            <div class="movie-card">
                 <div class="movie-item">
                     <img src="/assets/img/<?= $movie['image']; ?>" alt="<?= $movie['title']; ?>">
-                    <div class="movie-info">
-                        <h3><?= $movie['title']; ?></h3>
-                        <p>Rating: <?= $movie['rating']; ?></p>
-                    </div>
                 </div>
-            <?php endif; ?>
-        <?php endforeach; ?>
-    </div>
+                <div class="movie-info">
+                    <h3><?= $movie['title']; ?></h3>
+                    <p>Rating: <?= $movie['rating']; ?></p>
+                </div>
+            </div>
+        <?php endif; ?>
+    <?php endforeach; ?>
+</div>
+
 </body>
 </html>
