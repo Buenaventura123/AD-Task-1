d663a2a54220fbe4b4abce7e813851d195d01c5a
Buenaventura123 <202310536@fit.edu.ph>
2025-05-12 15:36:44 +0800

    refactor(style): increase the gaps of the card

--- page/home/index.php
+++ page/home/index.php
@@ -1,25 +1,26 @@
-<?php
-include '../../utils/movies.utils.php';
-$title = 'All Movies';
-include '../../components/navbar.component.php';
-?>
+<?php include '../../utils/movies.utils.php'; ?>
+<?php include '../../components/navbar.component.php'; ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <title>All Movies</title>
     <link rel="stylesheet" href="../../assets/css/style.css">
-    <title><?= $title ?></title>
 </head>
 <body>
-    <div class="movie-list">
-        <?php foreach ($movies as $movie): ?>
+
+<div class="movie-list">
+    <?php foreach ($movies as $movie): ?>
+        <div class="movie-card">
             <div class="movie-item">
                 <img src="/assets/img/<?= $movie['image']; ?>" alt="<?= $movie['title']; ?>">
-                <div class="movie-info">
-                    <h3><?= $movie['title']; ?></h3>
-                    <p>Rating: <?= $movie['rating']; ?></p>
-                </div>
             </div>
-        <?php endforeach; ?>
-    </div>
+            <div class="movie-info">
+                <h3><?= $movie['title']; ?></h3>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>
+
 </body>
 </html>
