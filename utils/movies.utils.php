2528d80d4091e3fed4f91ae9832eb3f6dec2cf8b
Buenaventura123 <202310536@fit.edu.ph>
2025-05-12 16:06:07 +0800

    chore(more movies): added more movies

--- utils/movies.utils.php
+++ utils/movies.utils.php
@@ -11,20 +11,54 @@ $movies = [
         "image" => "image2.jpg"
     ],
     [
-        "title" => 
-        "Death of a Unicorn", 
-        "rating" => 6.8, "image" => 
-        "image3.jpg"
+        "title" => "Death of a Unicorn", 
+        "rating" => 6.8, 
+        "image" => "image3.jpg"
     ],
     [
         "title" => "The Last of Us", 
-        "rating" => 8.7, "image" => 
-        "image4.jpg"
+        "rating" => 8.7, 
+        "image" => "image4.jpg"
     ],
     [
         "title" => "Dirk Gently's Holistic Detective Agency", 
         "rating" => 8.2, 
         "image" => "image5.jpg"
-        ]
+    ],
+    [
+        "title" => "The Dark Knight", 
+        "rating" => 9.0, 
+        "image" => "image5.jpg"
+    ],
+    [
+        "title" => "Inception ", 
+        "rating" => 8.8, 
+        "image" => "image5.jpg"
+    ],
+    [
+        "title" => "The Matrix", 
+        "rating" => 8.7, 
+        "image" => "image5.jpg"
+    ],
+    [
+        "title" => "Interstellar ", 
+        "rating" => 8.6, 
+        "image" => "image5.jpg"
+    ],
+    [
+        "title" => "Teenage Mutant Ninja Turtles (2014)", 
+        "rating" => 5.8, 
+        "image" => "image5.jpg"
+    ],
+    [
+        "title" => "Back to the Future", 
+        "rating" => 8.5, 
+        "image" => "image5.jpg"
+    ],
+    [
+        "title" => "Transformers: The Last Knight", 
+        "rating" => 5.2, 
+        "image" => "image5.jpg"
+    ]
 ];
 ?>
